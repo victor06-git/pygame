7e4eba8314702b92b87721f6cb5f05f67add61c0
victor06-git <asensiovector@gmail.com>
2024-11-03 13:18:09 +0100

    #

--- deures7.py
+++ deures7.py
@@ -62,7 +62,21 @@ def app_draw():
     
     # Dibuixar la graella de coordenades (llibreria utils)
     cuadricula.draw_grid(pygame, screen, 50)
-    # dibuix
+    # dibuix  
+    colors = [(127, 184, 68), (240, 187, 64), (226, 137, 50), (202, 73, 65), (135, 65, 152), (75, 154, 217)]
+    x1,x2,x3,x4 = 50,100,100,50
+    x5 = 75
+    for q in range(0, len(colors)):    
+        #quadrats
+        pygame.draw.polygon(screen, colors[q], [(x1,50),(x2,50),(x3,100),(x4,100)])
+        x1  += 100
+        x2  += 100
+        x3  += 100
+        x4  += 100
+        #cercles
+        pygame.draw.circle(screen, colors[q], (x5,175), 25, 3)
+        x5 +=  100
+    
     def draw_polygon(screen, color, center, radius, num_vertices, angle_offset=(math.pi / 3)):
         points = [
             (
@@ -73,27 +87,13 @@ def app_draw():
         ]
         pygame.draw.polygon(screen, color, points)
     
-    x1,x2,x3,x4 = 50,100,100,50
-    color = 0
-    for _ in range(6):    
-        if color == 0:
-            color = GREEN
-        elif color == 1:
-            color = YELLOW
-        elif color == 2:
-            color = ORANGE
-        elif color == 3:
-            color = RED
-        elif color == 4:
-            color = PURPLE
-        elif color == 5:
-            color  = BLUE
+    greys = [(0,0,0),(25,25,25),(50,50,50),(75,75,75),(100,100,100),(125,125,125)]
+    for q in range(0, len(greys)):
+        draw_polygon(screen, greys[q], (x5, 275), 25, 3, 1)
+    
 
-        pygame.draw.polygon(screen, color, [(x1,50),(x2,50),(x3,100),(x4,100)])
-        x1  += 100
-        x2  += 100
-        x3  += 100
-        x4  += 100
+        
+        
 
 
 
