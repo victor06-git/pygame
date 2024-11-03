287cd28185e5d1b2636fe0e3eade3c9425d6545d
victor06-git <asensiovector@gmail.com>
2024-11-03 16:39:35 +0100

    #

--- deures11.py
+++ deures11.py
@@ -22,6 +22,9 @@ clock = pygame.time.Clock()
 # Definir la finestra
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Window Title')
+dir_x = 'none'
+pos_x = 100
+radio = 25
 
 # Bucle de l'aplicació
 def main():
@@ -40,74 +43,63 @@ def main():
 
 # Gestionar events
 def app_events():
-    global dir_x, dir_y
+    global dir_x
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # Botó tancar finestra
             return False
         elif event.type == pygame.KEYDOWN:
-            if  event.key == pygame.K_UP:
-                dir_y = 'up'
-            elif  event.key == pygame.K_DOWN:
-                dir_y = 'down'
-            elif   event.key == pygame.K_LEFT:
-                dir_x = 'left'
-            elif   event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 dir_x = 'right'
+            elif event.type == pygame.K_LEFT:
+                dir_x =  'left'
         elif  event.type == pygame.KEYUP:
-            if  event.key == pygame.K_UP:
-                if dir_y == 'up':
-                    dir_y = 'none'
-            elif   event.key == pygame.K_DOWN:
-                if dir_y == 'down':
-                    dir_y ='none'
-            elif    event.key == pygame.K_LEFT:
-                if  dir_x == 'left':
-                    dir_x = 'none'
-            elif     event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 if  dir_x == 'right':
                     dir_x = 'none'
+            elif event.key == pygame.K_LEFT:
+                if   dir_x == 'left':
+                    dir_x = 'none'
+
     return True
 
 # Fer càlculs
 def app_run():
-    global dir_x, dir_y, pos_x, pos_y
+    global dir_x, pos_x, radio
 
     delta_time = clock.get_time()/1000.0
 
-    speed = 50
+    speed = 100
     displacement = speed * delta_time
-
+    width = screen.get_width() - radio
+    
     if (dir_x == "right"):
         pos_x = pos_x + displacement
-        if (pos_x > 450):
-            pos_x = 450
-    elif (dir_x ==  "left"):
+        if (pos_x > width):
+            pos_x = width
+    elif (dir_x == "left"):
         pos_x = pos_x - displacement
-        if (pos_x < 0):
-            pos_x = 0
-    elif  (dir_y == "down"):
-        pos_y = pos_y - displacement
-        if (pos_y > 450):
-            pos_y = 450
-    elif (dir_y == "up"):
-        pos_y = pos_y + displacement
-        if (pos_y < 0):
-            pos_y = 0
+        if (pos_x < radio):
+            pos_x = radio
+    radio = 10 + (pos_x / 8)
 
 
 
 
 # Dibuixar
 def app_draw():
-    global pos_x, pos_y
+    global pos_x
     # Pintar el fons de blanc
     screen.fill(WHITE)
     
+    font = pygame.font.SysFont("Arial", 24)
+    text = font.render('Apreta les tecles (left/right)', True, BLACK)
+    screen.blit(text, (50, 50))
     # Dibuixar la graella de coordenades (llibreria utils)
     cuadricula.draw_grid(pygame, screen, 50)
     # dibuix
-    radio = 10 + (pos_x / 8)
-    pygame.draw.circle(screen, BLACK,  (int(pos_x), int(pos_y)), radio)
+    
+    pygame.draw.circle(screen, BLACK,  (pos_x, 250), radio)
 
     
 
