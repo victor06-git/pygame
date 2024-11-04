f846b47321bd7c624db8fb6910510d989f989d6d
victor06-git <asensiovector@gmail.com>
2024-11-04 17:14:33 +0100

    #

---
+++ deures15.py
@@ -0,0 +1,70 @@
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import cuadricula
+
+# Definir colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE  = (50, 120, 200)
+
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Definir la finestra
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Window Title')
+
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Botó tancar finestra
+            return False
+    return True
+
+# Fer càlculs
+def app_run():
+    pass
+
+# Dibuixar
+def app_draw():
+    
+    # Pintar el fons de blanc
+    screen.fill(WHITE)
+    
+    # Dibuixar la graella de coordenades (llibreria utils)
+    cuadricula.draw_grid(pygame, screen, 50)
+    # dibuix
+    columns = 21
+    hue_step = (360 / columns)
+    for column in range(0, columns):
+        x = 50 + column * 25
+        hue = hue_step * column
+
+        color = cuadricula.hsl_to_rgb(hue,  1.0, 0.5)
+        pygame.draw.rect(screen, color, (x, 200, 25, 25))
+
+    # Actualitzar el dibuix a la finestra
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()
